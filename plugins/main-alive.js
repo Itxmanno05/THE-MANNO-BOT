d8580c33ae0df2d36893b95e73abe89982cf9ba4
mano125 <153306254+mano125@users.noreply.github.com>
2024-01-25 16:19:09 +0500

    Update main-alive.js

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -2,10 +2,10 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
 
     // Sound
     let name = m.pushName || conn.getName(m.sender)
-    var vn = "https://github.com/PRINCE-GDS/THE-PRINCE-BOT/raw/main/Assets/mp3/Audio5.mp3"
+    var vn = "https://cdn.jsdelivr.net/gh/mano125/GURU-BOT@main/Assets/mp3/usman.mp3"
     let url = "https://github.com/mano125/THE-PRINCE-BOT"
     let murl = "https://youtu.be/DibiLc17dh0?si=xp9bQ-_frEyDB1-i"
-    let img = "https://telegra.ph/file/8f8adf1235e3abcd287a1.png"
+    let img = "https://telegra.ph/file/b25d011e37f5a8428fcfd.jpg"
     let con = { key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
     let doc = {
         audio: {
