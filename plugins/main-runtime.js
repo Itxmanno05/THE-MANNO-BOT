2441faf390d615729fa8c9b9a240a7343ea149a8
mano125 <153306254+mano125@users.noreply.github.com>
2024-01-25 16:13:23 +0500

    Update main-runtime.js

--- plugins/main-runtime.js
+++ plugins/main-runtime.js
@@ -10,7 +10,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
       }) * 1000
     }
     let muptime = clockString(_muptime)
-   m.reply(`🏮 *Bot active duration*  \n\n${muptime}`) 
+   m.reply(`📅 *𝐂𝐘𝐁𝐄𝐑_𝐖𝐀𝐑𝐑𝐈𝐎𝐑 *  \n\n${muptime}`) 
 }
 handler.help = ['runtime']
 handler.tags = ['main']
